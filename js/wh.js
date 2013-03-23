a7c8740a756f2267ad05834484e3a5c06fbefc89
feivel <feivel@sdf.org>
2013-03-23 19:49:17 -0400

    js & css fixes

--- js/wh.js
+++ js/wh.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   window.ws = createSocket();
-  window.msg_count = 0;
 
   window.message_view = Backbone.View.extend({
     tagName: "div",
@@ -8,11 +7,6 @@ $(document).ready(function() {
     render: function(t) {
       this.$el.html(this.template(t));
       this.$el.addClass("wh_msg");
-      if((msg_count % 2) == 0) {
-        this.$el.addClass("even");
-      } else {
-        this.$el.addClass("odd");
-      }
       return this;
     }
   });
@@ -20,7 +14,6 @@ $(document).ready(function() {
   window.message_list_view = Backbone.View.extend({
     el: $("#wh_box"),
     show_message: function(txt) {
-      msg_count += 1;
       this.$el.append((new message_view).render(txt).$el);
       var cs = this.$el.children();
       if(cs.toArray().length > 30) cs.first().remove();
@@ -43,13 +36,11 @@ $(document).ready(function() {
 });
 
 function createSocket() {
-  ws_url = "ws://ec2-54-244-180-87.us-west-2.compute.amazonaws.com:8888"
+  ws_url = "ws://ec2-54-244-180-87.us-west-2.compute.amazonaws.com";
   var s = new WebSocket(ws_url);
   s.onmessage = function (msg) { messages.show_message(msg.data.replace(/\n/g,"<br />")); };
   s.onclose = function (msg) { messages.show_message("### connection closed ###"); };
   return s;
 }
-
-
 
 
